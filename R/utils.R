b611412636d582b71d162cf4d59e41c7e6f7a470
Petar Glazar <petar.glazar@gmail.com>
2016-11-30 15:55:48 +0100

    calculate relative expression form raw Cts

--- R/utils.R
+++ R/utils.R
@@ -28,3 +28,70 @@ setMethod("setDesign",
             return(pcr)
           }
 )
+
+
+# ---------------------------------------------------------------------------- #
+#' relExp
+#'
+#' calculate relative expression for a qPCR run
+#'
+#' details
+#'
+#' @param pcr qPCR object to work on
+#' @param groups character vector of sample groups
+#'
+#' @importFrom xlsx read.xlsx
+#' @importFrom data.table data.table setnames
+#' @importFrom reshape2 melt dcast
+#' @export
+setGeneric(
+  name="relExp",
+  def=function(pcr, ...) {
+    standardGeneric("relExp")
+  }
+)
+setMethod("relExp",
+          signature("qPCR"),
+          definition=function(pcr, ref_sample, ref_target) {
+
+            if (!(ref_sample %in% pcr@data$sample) | !(ref_target %in% pcr@data$target)) {
+              stop("reference sample or target gene not present in raw data")
+            }
+
+            DT.raw <- pcr@data[,.(well, sample, target, Ct)]
+            DT <- cbind(data.table(melt(dcast(DT.raw, target~sample, mean))),
+                        data.table(melt(dcast(DT.raw, target~sample, sd))))
+            setnames(DT, c("target", "sample", "Ct_mean", "target2", "sample2", "Ct_sd"))
+            if (identical(DT$target, DT$target2) & identical(DT$sample, DT$sample2)) {
+              DT <- DT[,.(sample, target, Ct_mean, Ct_sd)]
+            } else {
+              stop("something is fishy with sample and/or target labels. blame the developer.")
+            }
+            DT <- DT[order(target)]
+
+            # the table has to be properly sorted for this!
+            DT$dCt <- DT$Ct_mean -  DT[target == ref_target]$Ct_mean
+            DT$dCt_sd <- sqrt(DT$Ct_sd**2 + DT[target == ref_target]$Ct_sd**2)
+
+            ddcts <- numeric()
+            for (i in unique(DT$target)) {
+              ddcts <- append(ddcts, DT[target==i]$dCt - DT[target==i & sample==ref_sample]$dCt)
+            }
+            DT$ddCt <- ddcts
+
+            DT$RQ <- 2**(-DT$ddCt)
+            DT$RQmin <- 2**(-DT$ddCt - DT$dCt_sd)
+            DT$RQmax <- 2**(-DT$ddCt + DT$dCt_sd)
+
+            pcr@pdata <- DT
+
+            return(pcr)
+          }
+)
+
+
+
+
+
+
+
--- R/workingClasses.R
+++ R/workingClasses.R
@@ -1,7 +1,17 @@
+# dirty patch to be able to use data.table in class slots
+setOldClass(c('data.frame'))
+setOldClass(c('data.table', 'data.frame'))
+
 #' An S4 class to represent a qPCR experiment.
 #'
 #' @slot data     data.table with qPCR data
 #' @slot metadata data.table with experimental metadata
+#' @slot design   factor vector of sample groupings for barplot coloring
+#' @slot pdata    processed data (e.g. recalculated relative expression)
+#' @importFrom data.table data.table
 setClass("qPCR",
-         slots = c(data="data.table", metadata="data.table", design="factor")
+         slots = c(data      = "data.table",
+                   metadata  = "data.table",
+                   design    = "factor",
+                   pdata     = "data.table")
 )
